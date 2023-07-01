a5a92d3bace19690a8ce7ff4ce1f5739a2463e27
Rafiur Rahman Saikat <md.rafiur.rahman.saikat@gmail.com>
2023-07-01 15:06:41 +0600

    Navbar Added

---
+++ components/CustomButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButtom = ({
+  title,
+  btnType,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      className={`custom-btn ${containerStyles}`}
+      type={btnType || "button"}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButtom;
