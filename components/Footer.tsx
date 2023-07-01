418e3d51f0d16e7f8f5ae7966fa5f30b1551fe59
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-07-01 15:17:51 +0600

    Footer Added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
-
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div>
+        <h1></h1>
+      </div>
+      Footer
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
