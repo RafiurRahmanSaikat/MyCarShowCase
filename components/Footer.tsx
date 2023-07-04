6bc01c3a950c28a07061f212fa4c2ea2c03889dd
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-07-04 20:37:40 +0600

    Foter Added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,46 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <h1></h1>
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className=" max-md:flex-col justify-between flex-wrap gap-5  sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start gap-6 items-start">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={118}
+              height={18}
+              className="object-contain"
+            />
+          </Link>
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All Rights Reserved &copy;
+          </p>
+        </div>
+        <div className="flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-20">
+          {footerLinks.map((item) => (
+            <div key={item.title} className="flex flex-col gap-6 text-base min-w-[170px]">
+              <h3 className="font-bold">{item.title}</h3>
+              <div className="flex flex-col gap-5">
+                {item.links.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.url}
+                    className="text-gray-500"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+              
+            </div>
+          ))}
+        </div>
       </div>
-      Footer
     </footer>
   );
 };
